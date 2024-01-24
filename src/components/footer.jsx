ee6560336073e028a552f2fb9a444fcf5c110ed3
yszww <74343003+yszww@users.noreply.github.com>
2024-01-24 13:51:57 +0800

    Update footer.jsx

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -36,7 +36,7 @@ const Footer = () => {
         <p>
           Copyright&nbsp;&copy;&nbsp;2020&nbsp;-&nbsp;{new Date().getFullYear()}
           &nbsp;
-          <CustomLink to="https://www.imsyy.top/" text="無名" />
+          <CustomLink to="https://whys20.com" text="余生" />
           {siteIcp ? (
             <React.Fragment>
               &nbsp;|&nbsp;
